26d083c9548335cb10f5533fc491da27c0b7e04d
bilirz <2779264714@qq.com>
2022-12-08 01:08:38 +0800

    feat: inedx user list

--- handler/index.py
+++ handler/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, redirect
 import pymongo
 
 bp = Blueprint('index', __name__)
@@ -13,7 +13,9 @@ def index():
     if session['status'] == '游客':
         return render_template('user/no.html', sname=session['user'])
     else:
+        mongo['db_li']['users'].delete_one({'name': None})
+        users = mongo['db_li']['users'].find()
         redblack = mongo['db_li']['redblack'].find().sort('time', -1).limit(3)
         days = mongo['db_li']['days'].find().sort('time', -1).limit(3)
         pk = mongo['db_li']['pk'].find().sort('coin', -1).limit(3)
-        return render_template('index.html', sname=session['user'], redblack=redblack, days=days, pk=pk)
+        return render_template('index.html', sname=session['user'], users=users, redblack=redblack, days=days, pk=pk)
